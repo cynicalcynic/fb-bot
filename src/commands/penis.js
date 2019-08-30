afac2e18f81e1df074a936daa861056cb8594090
Michał Świderski <michal.sw12@gmail.com>
2019-08-31 01:37:42 +0200

    enlarged penis XD

--- src/commands/penis.js
+++ src/commands/penis.js
@@ -1,9 +1,16 @@
 const Command = require('../command.js');
 const {random} = require('../utils/utils.js');
 
-module.exports = new Command(async (args) => {
+module.exports = new Command(async (args, message, bot) => {
+    let target;
+    if(!args[0]){
+        const {name} = await bot.client.getUserInfo(message.authorId);
+        target = name;
+    }
+    else
+        target = args.join(' ');
     return {
-        text: `8${'='.repeat(random(1, 20))}D`
+        text: `*${target}'s*\n\n8${'='.repeat(random(1, 20))}D`
     }
 }, 
 {
