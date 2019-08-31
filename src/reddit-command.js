1893784c6ec23a0eea8b517746012baa5f6527cc
Michał Świderski <michal.sw12@gmail.com>
2019-08-31 22:56:25 +0200

    added error handler

--- src/reddit-command.js
+++ src/reddit-command.js
@@ -7,7 +7,12 @@ class RedditCommand{
     }
     async execute(args, message, bot){
 
-        let res = await request('https://www.reddit.com' + this.props.endpoint);
+        try{
+            let res = await request('https://www.reddidfgdfgft.com' + this.props.endpoint);
+        }
+        catch(e){
+            return {text : 'meme machine broke\n' + e}
+        }
         const posts = JSON.parse(res).data.children.filter(post=>post.data.post_hint == 'image');
         const meme = arrayRandom(posts).data.url;
         return {
