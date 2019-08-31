1276546b67b405d9fa746634eea438b96e163bc3
Michał Świderski <michal.sw12@gmail.com>
2019-08-31 23:49:40 +0200

    lil fix

--- src/reddit-command.js
+++ src/reddit-command.js
@@ -6,17 +6,16 @@ class RedditCommand{
         this.props = props;
     }
     async execute(args, message, bot){
-
         try{
             let res = await request('https://www.reddit.com' + this.props.endpoint);
+            const posts = JSON.parse(res).data.children.filter(post=>post.data.post_hint == 'image');
+            const meme = arrayRandom(posts).data.url;
+            return {
+                attachment : [meme]
+            }
         }
         catch(e){
             return {text : 'meme machine broke\n' + e}
-        }
-        const posts = JSON.parse(res).data.children.filter(post=>post.data.post_hint == 'image');
-        const meme = arrayRandom(posts).data.url;
-        return {
-            attachment : [meme]
         }
     }
 }
