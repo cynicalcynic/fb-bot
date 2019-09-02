f27cd467669ba8c8fd15c22bfa66cf0479ebeae4
Michał Świderski <michal.sw12@gmail.com>
2019-09-03 01:10:48 +0200

    added kremówka

--- src/commands/jp2.js
+++ src/commands/jp2.js
@@ -3,6 +3,6 @@ const {arrayRandom} = require('../utils/utils.js');
 
 module.exports = new Command( 
 {
-    triggers : ['jp2', 'jp2gmd', '2137'],
+    triggers : ['jp2', 'jp2gmd', '2137', 'papaj', 'kremówka'],
     endpoint : '/r/2137.json?t=day&limit=100'
 });
