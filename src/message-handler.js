20a4f4c379a44ca5b0258e01d6b30f065a06a8ed
Michał Świderski <michal.sw12@gmail.com>
2019-09-02 19:33:31 +0200

    code cleanup

--- src/message-handler.js
+++ src/message-handler.js
@@ -7,33 +7,33 @@ const uuid = require('uuid/v1');
 
 module.exports = async function messageHandler(message){
     let {success, args, cmd} = parseMessage(message.message, '!');
-    if(success){
-        let command = this.commands.find((command) => command.props.triggers.includes(cmd));
-        if(command !== undefined){
-            let {text, attachment} = await command.execute(args, message, this);
-            if(text !== undefined)
-                this.client.sendMessage(message.threadId, text);
+    if(!success) return;
 
-            if(attachment !== undefined){
-                const tempDir = join(__dirname, 'tmp');
-                ensureDirSync(tempDir);
-                for(let img of attachment){
-                    let filePath;
-                    if(img.startsWith('http')){
-                        filePath = join(tempDir, uuid());
-                        await downloadFile(img, filePath);
-                        this.client.sendAttachmentFile(message.threadId, filePath).then(()=>unlink(filePath));
-                    }
-                    else{
-                        filePath = join(__dirname, img);
-                        this.client.sendAttachmentFile(message.threadId, filePath);
-                    }
+    let command = this.commands.find((command) => command.props.triggers.includes(cmd));
+    if(command !== undefined){
+        let {text, attachment} = await command.execute(args, message, this);
+        if(text !== undefined)
+            this.client.sendMessage(message.threadId, text);
+
+        if(attachment !== undefined){
+            const tempDir = join(__dirname, 'tmp');
+            ensureDirSync(tempDir);
+            for(let img of attachment){
+                let filePath;
+                if(img.startsWith('http')){
+                    filePath = join(tempDir, uuid());
+                    await downloadFile(img, filePath);
+                    this.client.sendAttachmentFile(message.threadId, filePath).then(()=>unlink(filePath));
+                }
+                else{
+                    filePath = join(__dirname, img);
+                    this.client.sendAttachmentFile(message.threadId, filePath);
                 }
             }
         }
-        else{
-            this.client.sendMessage(message.threadId, 'I dont know what you want me to do faggot');
-        }
+    }
+    else{
+        this.client.sendMessage(message.threadId, 'I dont know what you want me to do faggot');
     }
 }
 
