95cfcdffcf08605b0c55e65d101ad8d3830bf5a8
Michał Świderski <michal.sw12@gmail.com>
2019-09-02 23:22:09 +0200

    reddit api requests are now cached

--- src/reddit-command.js
+++ src/reddit-command.js
@@ -6,17 +6,28 @@ class RedditCommand{
         this.props = props;
     }
     async execute(args, message, bot){
-        try{
-            let res = await request('https://www.reddit.com' + this.props.endpoint);
-            const posts = JSON.parse(res).data.children.filter(post=>post.data.post_hint == 'image');
-            const meme = arrayRandom(posts).data.url;
-            return {
-                attachment : [meme]
+
+        let res;
+        let cached = await bot.redis.get(this.props.endpoint).then(r=>JSON.parse(r));
+        if(cached){
+           res = cached;
+        }
+        else{
+            try{
+                res = await request('https://www.reddit.com' + this.props.endpoint);
             }
+            catch(e){
+                return {text : 'meme machine broke\n' + e}
+            }
+            
+            bot.redis.set(this.props.endpoint, JSON.stringify(res), 'EX', 15 * 60);
         }
-        catch(e){
-            return {text : 'meme machine broke\n' + e}
+
+        const posts = JSON.parse(res).data.children.filter(post=>post.data.post_hint == 'image');
+        return {
+            attachment: [arrayRandom(posts).data.url]
         }
+        
     }
 }
 
