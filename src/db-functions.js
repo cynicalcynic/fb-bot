1df0b3a37056ff30b70ec5a2b12ed17c37ece7ea
Michał Świderski <michal.sw12@gmail.com>
2019-09-04 02:50:24 +0200

    added getThread and User functions

--- src/db-functions.js
+++ src/db-functions.js
@@ -1,21 +1,47 @@
+const {promisify} = require('util');
+
 module.exports = (bot) => ({
     
-    getThreadConfig(threadId){
+    getThreadConfig(threadID){
         const defaultConfig = {
             prefix : '!',
             nsfw : false,
             swears : false
         }
 
-        if(!bot.config.has(threadId)){
-            bot.config.set(threadId, defaultConfig);
+        if(!bot.config.has(threadID)){
+            bot.config.set(threadID, defaultConfig);
         }
-        return bot.config.get(threadId);
+        return bot.config.get(threadID);
     },
 
-    setThreadConfig(threadId, prop, value){
-        const config = this.getThreadConfig(threadId);
+    setThreadConfig(threadID, prop, value){
+        const config = this.getThreadConfig(threadID);
         config[prop] = value;
+    },
+
+    async getThreadInfo(threadID){
+        const res = await bot.redis.get(threadID);
+        if(!res){
+            const info = await promisify(bot.client.getThreadInfo)(threadID);
+            bot.redis.set(threadID, JSON.stringify(info), 'EX', 60 * 60);
+            return info;
+        }
+        else{
+            return JSON.parse(res);
+        }
+    },
+
+    async getUserInfo(userID){
+        const res = await bot.redis.get(userID);
+        if(!res){
+            const info = await promisify(bot.client.getUserInfo)(userID);
+            bot.redis.set(userID, JSON.stringify(info)[userID], 'EX', 60 * 60);
+            return info;
+        }
+        else{
+            return JSON.parse(res)[userID];
+        }
     }
 
 });
